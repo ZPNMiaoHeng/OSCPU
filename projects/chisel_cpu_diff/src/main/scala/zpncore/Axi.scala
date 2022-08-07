ae928a6acbba17018ca834b06fc48e0062b5eda0
tracer-oscpu2021 <tracer@oscpu.org>
2022-08-07 18:50:23 +0800

    Miao Heng 4021110045    Static hostname: Worker          Icon name: computer-vm            Chassis: vm         Machine ID: 055b8a680df641879bbbd3a60070dcc3            Boot ID: c7f63a92370141739a7777c19ff7fa53     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-43-generic       Architecture: x86-64  18:50:23 up  8:08,  4 users,  load average: 0.43, 0.35, 0.30

--- projects/chisel_cpu_diff/src/main/scala/zpncore/Axi.scala
+++ projects/chisel_cpu_diff/src/main/scala/zpncore/Axi.scala
@@ -52,14 +52,8 @@ class AxiLite2Axi  extends Module {
   }
 
     // ------------------Write Transaction------------------
-/*
-  val axi_addr = 0.U
-  when( r_state === r_inst_addr ) {
-     axi_addr := in1.inst_addr & "hffff_fff0".U(32.W)
-  }
-*/
-  val axi_addr = Mux(r_state === r_inst_addr, in1.inst_addr & "hffff_fff0".U(32.W), 0.U)  // Byte alignment
-//  val axi_addr = Mux(r_state === r_inst_addr, (in1.inst_addr + 4.U) & "hffff_fff0".U(32.W), 0.U)  // Byte alignment
+//  val axi_addr = Mux(r_state === r_inst_addr, in1.inst_addr & "hffff_fff0".U(32.W), 0.U)  // Byte alignment
+  val axi_addr = in1.inst_addr & "hffff_fff0".U(32.W) // Byte alignment
 
   out.ar.valid := (r_state === r_inst_addr)
   out.ar.bits.addr := axi_addr
