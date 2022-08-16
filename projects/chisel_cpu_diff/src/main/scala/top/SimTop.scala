3e3a60d36b991da3369a5f1c2d8b56164254fd51
4021110045-Miao Heng <1098011272@qq.com>
2022-08-16 18:07:42 +0800

    写完ICache,准备测试

--- projects/chisel_cpu_diff/src/main/scala/top/SimTop.scala
+++ projects/chisel_cpu_diff/src/main/scala/top/SimTop.scala
@@ -16,16 +16,21 @@ class SimTop extends Module {
   })
 
   val core = Module(new Core)
+  val icache = Module(new Icache)
 
   val mem = Module(new Ram2r1w)
   val top = Module(new AxiLite2Axi)
-  top.io.imem <> core.io.imem
+
+//  top.io.imem <> core.io.imem
+  core.io.imem  <> icache.io.imem
+  icache.io.out <> top.io.imem
 
   io.memAXI_0.aw <> top.io.out.aw
   io.memAXI_0.w  <> top.io.out.w
   io.memAXI_0.b  <> top.io.out.b
   io.memAXI_0.ar <> top.io.out.ar
   io.memAXI_0.r  <> top.io.out.r
+  
 //  mem.io.imem <> core.io.imem
   mem.io.dmem <> core.io.dmem
 
