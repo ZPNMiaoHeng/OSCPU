84e80d7e5c9617f4b0f9144a9a80bafc9697887a
4021110045-Miao Heng <1098011272@qq.com>
2022-08-20 14:00:51 +0800

    重写IF模块

--- projects/chisel_cpu_diff/src/main/scala/zpncore/fronted/InstFetch.scala
+++ projects/chisel_cpu_diff/src/main/scala/zpncore/fronted/InstFetch.scala
@@ -14,31 +14,51 @@ class InstFetch extends Module {
 
     val IFDone = Output(Bool())                       //* 有效时才取到指令。后面流水级才能运行，否则处于暂停状态
   })
-  val pc = RegInit("h8000_0000".U(WLEN.W))            //* nextPC = 0x8000_0000,可以取到正确指令
+  val pc = RegInit("h7fff_fffc".U(WLEN.W))            //* nextPC = 0x8000_0000,可以取到正确指令
+//  val pc = RegInit("h8000_0000".U(WLEN.W))            //* nextPC = 0x8000_0000,可以取到正确指令
   val inst = RegInit(0.U(WLEN.W))
   val IFDone = RegInit(false.B)
 
-  io.imem.inst_valid := true.B                        //* IF valid一直有效，请求AXI传输指令
-  val fire = io.imem.inst_valid && io.imem.inst_ready //* 握手成功，从总线上取出指令
+  io.imem.inst_valid := !io.stall                        //* IF valid暂停无效，请求AXI传输指令
+  val fire = io.imem.inst_valid && io.imem.inst_ready            //* 握手成功，从总线上取出指令
 // 握手成功，从总线上取到指令，更新寄存器PC与inst
+  val ifInst = Mux(fire, io.imem.inst_read, inst)
+  val ifPC = Mux(fire, 
+              Mux(io.pcSrc =/= 0.U, io.nextPC, pc + 4.U),
+               pc)
+  pc := ifPC                                          //* 更新pc/inst寄存器值,并保持当前寄存器状态 
+  inst := ifInst
+
+  io.imem.inst_req := REQ_READ
+  io.imem.inst_addr := ifPC.asUInt()
+  io.imem.inst_size := SIZE_W
+  io.IFDone := fire                                   //* fire有效，取到inst，取指阶段完成
+/*
+  val pc = RegInit("h8000_0000".U(WLEN.W))            // nextPC = 0x8000_0000,可以取到正确指令
+  val inst = RegInit(0.U(WLEN.W))
+  val IFDone = RegInit(false.B)
+
+  io.imem.inst_valid := !io.stall//true.B                        // IF valid暂停无效，请求AXI传输指令
+  val fire = io.imem.inst_valid && io.imem.inst_ready            // 握手成功，从总线上取出指令
+
   val ifInst = Mux(fire && (!io.stall), io.imem.inst_read, inst)
   val ifPC = Mux(IFDone,
             Mux(io.pcSrc === 0.U, 
               Mux(io.stall, pc, pc + 4.U),
                 io.nextPC),
                   pc)
-  IFDone := fire                                      //* PC改变需要打一拍，才能获得当前inst
-  pc := ifPC                                          //* 更新pc/inst寄存器值,并保持当前寄存器状态 
+  IFDone := RegNext(fire)                                      // PC改变需要打一拍，才能获得当前inst
+  pc := ifPC                                          // 更新pc/inst寄存器值,并保持当前寄存器状态 
   inst := ifInst
 
   io.imem.inst_req := REQ_READ
   io.imem.inst_addr := pc.asUInt()
   io.imem.inst_size := SIZE_W
-  io.IFDone := fire                                   //* fire有效，取到inst，取指阶段完成
-
+  io.IFDone := fire                                   // fire有效，取到inst，取指阶段完成
+*/
 //------------------- IF ----------------------------
   io.out.valid    := fire
-  io.out.pc       := ifPC  //pc           //* pc需要打一拍等待ifinst取指
+  io.out.pc       := ifPC           //* pc需要打一拍等待ifinst取指
   io.out.inst     := ifInst
   io.out.typeL    := false.B
   io.out.aluA     := 0.U
