fa0a7509f910cfb3b907c2023482eeb6a72939d3
220031-Miao Heng <1098011272@qq.com>
2023-09-14 09:49:42 +0800

    modify port

--- projects/chisel_cpu_diff/src/main/scala/top/FPGATop.scala
+++ projects/chisel_cpu_diff/src/main/scala/top/FPGATop.scala
@@ -10,9 +10,7 @@ import chisel3.util._
 // class FPGAIO(implicit val p: ZpnCoreConfig) extends Bundle {
 class FPGAIO extends Bundle {
     val en = Input(Bool())
-    // val inst = Valid(Input(UInt(32.W)))
-    val cnt = Output(UInt(6.W))
-    // val cnt_top = Output(Bool())
+    val cnt = Output(UInt(4.W))
 }
 
 class FPGATop extends Module {
@@ -28,7 +26,7 @@ class FPGATop extends Module {
     val s_IDLE :: s_AR :: s_R :: Nil = Enum(3)
     val state = RegInit(s_IDLE)
 
-    val cnt = Counter(64)
+    val cnt = Counter(16)
     when(io.fpga.en) {
         cnt.inc()
     }
@@ -61,7 +59,8 @@ class FPGATop extends Module {
     val sR = state === s_R
     ar.ready := sAR && ar.bits.addr === ("h8000_0000".U)
     r.valid := sR && io.fpga.en
-    r.bits.data := Mux(sR && io.fpga.en, "h00000393_00208733_00000113_00000093".U, 0.U)
+    r.bits.data := Mux(sR && io.fpga.en, "h00000113_00000093".U, "h00000013_00000013".U)
+    // r.bits.data := Mux(sR && io.fpga.en, "h00000113_00000093".U, 0.U)
     r.bits.resp := DontCare
     r.bits.last := DontCare
 
@@ -72,51 +71,3 @@ class FPGATop extends Module {
     // 8000000c:	00000393          	li	t2,0
 }
 
-
-/* 
-    一次输入端口也是太多,需要129
-*/
-
-// class FPGAIO(implicit val p: ZpnCoreConfig) extends Bundle {
-//     // val en = Input(Bool())
-//     val inst = Valid(Input(UInt(128.W)))
-//     val cnt = Output(UInt(6.W))
-//     // val cnt_top = Output(Bool())
-// }
-
-// class FPGATop_in extends Module {
-//   val io = IO(new Bundle {
-//     val fpga = new FPGAIO
-//     // val axi = Flipped(new AxiIO)
-//   })
-
-//   val zpncore = Module(new SimTop)
-
-//     // zpncore.io.memAXI_0 <> io.axi
-//   zpncore.io.memAXI_0.aw <> Dontcare // top.io.out.aw
-//   zpncore.io.memAXI_0.w  <> Dontcare // top.io.out.w
-//   zpncore.io.memAXI_0.b  <> Dontcare // top.io.out.b
-// //   zpncore.io.memAXI_0.ar <> top.io.out.ar
-// //   zpncore.io.memAXI_0.r  <> top.io.out.r
-
-//     // val counter = RegInit(0.U(6.W))
-//     val cnt = Counter(64)
-//     when(io.fpga.inst.valid) {
-//         cnt.inc()
-//     }
-
-//     val cnt_value = cnt.value
-//     val ar = zpncore.io.memAXI_0.ar
-//     val r = zpncore.io.memAXI_0.r
-//     io.fpga.cnt := cnt_value
-//     when(zpncore.io.memAXI_0.ar.valid) {
-//         when(zpncore.io.memAXI_0.ar.addr == ("h8000_0000".U + 4 * cnt_value)) {   // Firsr inst
-//             ar.ready := true.B
-//         }
-//     }
-//     when(io.fpga.io.inst.valid) {
-//         r.valid := io.fpga.io.inst.valid
-//         r.
-//     }
-
-// }
